1b6507249a62bae409ca02696951f74747a09b93
Hoang Nguyen <hoangnguyen@vietswiss.com>
2023-05-30 13:06:07 +0700

    update script

--- test.sh
+++ test.sh
@@ -5,4 +5,5 @@ nodeVer=${testConfig#"$phpVer"}
 nodeVer=${nodeVer#_}
 echo "php ver: ${phpVer}"
 echo "node ver: ${nodeVer}"
+echo "PHP_VER=${phpVer} NODE_VER=${nodeVer}"
 # echo "con 1:${configs[1]}"
